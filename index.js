787200a4d43def52bd589b4d1dc43dedd25680cc
PrashantHanda011 <handaprashant719@gmail.com>
2022-01-12 17:10:40 +0530

    new

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(cookieParser());
 const localhost_url="http://localhost:3000"
 const netlify_url="https://school-management-sinox.netlify.app"
 
-app.use(cors({origin: {netlify_url},
+app.use(cors({origin: netlify_url,
   credentials: true}))
 
 app.options("*", cors());   
 
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', {netlify_url});
+   res.header('Access-Control-Allow-Origin', netlify_url);
    res.setHeader('Access-Control-Allow-Credentials',true);
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
