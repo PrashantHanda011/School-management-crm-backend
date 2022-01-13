4c2970a440cea05ada2e40b4af8dabd58d62a373
PrashantHanda011 <handaprashant719@gmail.com>
2022-01-13 12:11:59 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import auth from './Router/auth.js'
 import cors from 'cors'
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv, { config } from 'dotenv'
 import database from './db/conn.js'
 import cookieParser from 'cookie-parser';
 import admin from './Router/admin.js'
@@ -13,12 +13,20 @@ dotenv.config({path:'./config.env'})
 app.use(cookieParser());
 
 
-const origins = {
-  domain:[ "http://localhost:3000","https://school-management-sinox.netlify.app"]
-}
+var allowedDomains = ['http://localhost:3000', 'https://school-management-sinox.netlify.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },credentials: true
+}));
 
-app.use(cors({origin: origins,
-  credentials: true}))
 
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', origins);
