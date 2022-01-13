f90e1458b65576c2d46540c1cfaa05d2390a5ce2
PrashantHanda011 <handaprashant719@gmail.com>
2022-01-13 12:20:31 +0530

    new

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', allowedDomains);
-   res.setHeader('Access-Control-Allow-Credentials',true);
+   
    res.header('Access-Control-Allow-Origin', "*");
    next();
   });
