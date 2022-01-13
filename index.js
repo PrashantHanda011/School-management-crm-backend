7b86445e637e6c52338edca912d48bfed9253ba8
PrashantHanda011 <handaprashant719@gmail.com>
2022-01-13 12:26:12 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import auth from './Router/auth.js'
 import cors from 'cors'
 import express from 'express';
-import dotenv, { config } from 'dotenv'
+import dotenv from 'dotenv'
 import database from './db/conn.js'
 import cookieParser from 'cookie-parser';
 import admin from './Router/admin.js'
@@ -13,23 +13,16 @@ dotenv.config({path:'./config.env'})
 app.use(cookieParser());
 
 
-var allowedDomains = [ 'https://school-management-sinox.netlify.app'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // bypass the requests with no origin (like curl requests, mobile apps, etc )
-    if (!origin) return callback(null, true);
- 
-    if (allowedDomains.indexOf(origin) === -1) {
-      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },credentials: true
-}));
+const origins = {
+  domain:[ "https://school-management-sinox.netlify.app"]
+}
 
+app.use(cors({origin: origins,
+  credentials: true}))
 
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Origin', origins);
+   res.setHeader('Access-Control-Allow-Credentials',true);
    next();
   });
 
